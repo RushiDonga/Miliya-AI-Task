ff2913ca0568be5e414cdba27157e8b3f9adf8e0
Rushi Donga <rushi.donga.04@gmail.com>
2025-11-09 11:51:47 -0500

    on-failure added

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.get('/request-data', (req, res, next) => {
 });
 
 app.get('on-failure', (req, res, next) => {
-    res,json({message: 'We are unable to process your request at this time. Please try again later.'})
+    res.json({message: 'We are unable to process your request at this time. Please try again later.'})
 })
 
 app.listen(5000, () => console.log('Server running on PORT: 5000'))
