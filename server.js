bb2d0505c286473cbd620ade1fbd9900888daae2
Rushi Donga <rushi.donga.04@gmail.com>
2025-11-09 11:24:39 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express()
+
+app.get('/request-data', (req, res, next) => {
+    res.json({message: 'Welcome to Miliya AI. To get started, may I know your name and phone number, please?'})
+});
+
+app.listen(5000, () => console.log('Server running on PORT: 5000'))
