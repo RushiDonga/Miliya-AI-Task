83b7ef4325c6221d6d5fb70019ecf999cc142c6d
Rushi Donga <rushi.donga.04@gmail.com>
2025-11-09 11:49:03 -0500

    on-failure added

--- server.js
+++ server.js
@@ -6,4 +6,8 @@ app.get('/request-data', (req, res, next) => {
     res.json({message: 'Welcome to Miliya AI. To get started, may I know your name and phone number, please?'})
 });
 
+app.get('on-failure', (req, res, next) => {
+    res,json({message: 'We are unable to process your request at this time. Please try again later.'})
+})
+
 app.listen(5000, () => console.log('Server running on PORT: 5000'))
